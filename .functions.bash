c91a55da5a3b2e36744d9333303519021f1552ae
Josh Palay <jpalay@adharmonics.com>
2015-03-04 14:07:27 -0500

    Adds function to trigger desktop notification

--- .functions.bash
+++ .functions.bash
@@ -5,3 +5,24 @@ function my {
 function mcd {
     mkdir $1 && cd $1
 }
+
+function notify {
+    local usage="Usage: notify [title] message"
+    local title=''
+    local msg=''
+
+    if [ $# -lt 1 ]; then
+        echo $usage
+        return 1
+    elif [ $# -lt 2 ]; then
+        title=$1
+    elif [ $# -lt 3 ]; then
+        title=$1
+        msg=$2
+    else
+        echo $usage
+        return 1
+    fi
+
+    osascript -e "display notification \"$msg\" with title \"$title\""
+}
